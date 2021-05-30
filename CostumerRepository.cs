975acf2232926abbbf7f22a416729a889af20598
andakadriu <54242667+andakadriu@users.noreply.github.com>
2021-05-30 20:47:45 +0200

    Add files via upload

---
+++ CostumerRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RestaurantApp.Models;
+
+
+namespace RestaurantApp.Repositories
+{
+    public class CostumerRepository
+    {
+        private RestaurantDBEntities objRestaurantDbEntities;
+        public CostumerRepository()
+        {
+
+            objRestaurantDbEntities = new RestaurantDBEntities();
+
+        }
+        public IEnumerable<SelectListItem> GetAllCostumers()
+        {
+            var objSelectListItems = new List<SelectListItem>();
+            objSelectListItems = (from obj in objRestaurantDbEntities.Customers
+                                  select new SelectListItem()
+                                  {
+                                      Text = obj.CustomerName,
+                                      Value = obj.CustomerId.ToString(),
+                                      Selected = true
+
+                                  }
+                                 ).ToList();
+            return objSelectListItems;
+        }
+    }
+}
---
+++ OrderRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantApp.Repositories
+{
+    public class OrderRepository
+    {
+        private RestaurantDBEntities objRestaurantDbEntities;
+        public OrderRepository()
+        {
+
+            objRestaurantDbEntities = new RestaurantDBEntities();
+
+        }
+        public bool AddOrder(OrderViewModel objOrderViewModel)
+        {
+            Order objOrder = new Order();
+            objOrder.CostumerId = objOrderViewModel.CostumerId;
+            objOrder.FinalTotal = objOrderViewModel.FinalTotal;
+            objOrder.OrderDate = DateTime.Now;
+            objOrder.OrderNumber = String.Format("{0:ddmmmyyyyhhmmss}", DateTime.Now);
+            objOrder.PaymentTypeId = objOrderViewModel.PaymentTypeId;
+            objRestaurantDbEntities.Orders.Add(objOrder);
+            objRestaurantDbEntities.SaveChanges();
+            int OrderId = objOrder.OrderId;
+            foreach (var item in objOrderViewModel.ListOfOrderDetailViewModel)
+            {
+                OrderDetail objOrderDetail = new OrderDetail();
+                objOrderDetail.OrderId = OrderId;
+                objOrderDetail.Discount = item.Discount;
+                objOrderDetail.ItemId = item.ItemId;
+                objOrderDetail.Total = item.Total;
+                objOrderDetail.UnitPrice = item.UnitPrice;
+                objOrderDetail.Quantity = item.Quantity;
+                objRestaurantDbEntities.OrderDetails.Add(objOrderDetail);
+                objRestaurantDbEntities.SaveChanges();
+
+                Transaction objTransaction = new Transaction();
+                objTransaction.ItemId = item.ItemId;
+                objTransaction.Quantity = (-1) * item.Quantity;
+                objTransaction.TransactionDate = DateTime.Now;
+                objTransaction.TypeId = 2;
+                objRestaurantDbEntities.Transactions.Add(objTransaction);
+                objRestaurantDbEntities.SaveChanges();
+            }
+            return true;
+        }
+    }
+}
