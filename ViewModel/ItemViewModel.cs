b59b5f6c2db2ad339e97807107c7b822ef86d08c
EneaBrovina <75734637+EneaBrovina@users.noreply.github.com>
2021-05-30 13:44:03 -0500

    Add files via upload
    ViewModel

---
+++ ViewModel/ItemViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebRstaurant1._0.ViewModel
+{
+    public class ItemViewModel
+    {
+        public int ItemId { get; set; }
+
+        public string ItemName { get; set; }
+
+        public decimal ItemPrice { get; set; }
+
+
+    }
+}
